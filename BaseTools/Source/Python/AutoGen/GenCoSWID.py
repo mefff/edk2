8994cacebb352a5d2f1c13380efd96b26cf8a17f
Martin Fernandez <martin.fernandez@eclypsium.com>
2022-03-18 17:34:24 -0300

    Add current git hash to the uSWID per module .ini

--- BaseTools/Source/Python/AutoGen/GenCoSWID.py
+++ BaseTools/Source/Python/AutoGen/GenCoSWID.py
@@ -1,5 +1,6 @@
 import configparser
 import os.path as path
+import subprocess
 
 # TODO: This is kind of inspired from GenMake module although is very incomplete
 #
@@ -22,6 +23,17 @@ class ModuleCoSWID(object):
         self.Name = ModuleAutoGen.Name
         self.Guid = ModuleAutoGen.Guid
         self.Version = ModuleAutoGen.Version
+
+        try:
+            process = subprocess.run(["git", "rev-parse", "HEAD"],
+                                     capture_output=True,
+                                     check=True)
+            self.colloquial_version = process.stdout.decode("utf-8").strip()
+        # If either git is not in the system, the tree is not in a
+        # git repo or anything else just don't fail
+        except:
+            pass
+
         self.FilePath = path.join(ModuleAutoGen.BuildDir, self.Name + ".ini")
 
     def Generate(self):
@@ -32,6 +44,9 @@ class ModuleCoSWID(object):
             "software-name": self.Name,
             "software-version": self.Version,
         }
+
+        if not self.colloquial_version is None:
+            config["uSWID"]["colloquial-version"] = self.colloquial_version
 
         with open(self.FilePath, 'w') as f:
             config.write(f)
