af956b78f8d5b6fdff3ea252037031eaedad5d92
Martin Fernandez <martin.fernandez@eclypsium.com>
2022-05-30 14:37:26 -0300

    Clean up TODOs

--- BaseTools/Source/Python/AutoGen/GenCoSWID.py
+++ BaseTools/Source/Python/AutoGen/GenCoSWID.py
@@ -2,28 +2,19 @@ import configparser
 import os.path as path
 import subprocess
 
-# TODO: This is kind of inspired from GenMake module although is very incomplete
-#
-# Over there they use things like templates for generating files and
-# SaveOnChange for saving file's content plus a lot more of nice
-# things. This is just a start.
-#
-# TODO: Is the name *CoSWID precise?
-#
-# TODO: Does is make sense to have something else than ModuleCoSWID?
-#
-# In GenMake there are, the top level class: BuildFile and then
-# ModuleMakefile, CustomMakefile (not sure what this is),
-# PlatformMakefile and TopLevelMakefile
-#
-# Maybe TopLevelCoSWID could be interesenting, don't know where the
-# TopLevelMakefile ends up being
 class ModuleCoSWID(object):
+    '''Create a per-module ini file with SBOM related data
+
+    Also try to get the git HEAD commit hash if the project versioned
+    by git
+
+    '''
     def __init__(self, ModuleAutoGen):
         self.Name = ModuleAutoGen.Name
         self.Guid = ModuleAutoGen.Guid
         self.Version = ModuleAutoGen.Version
 
+        self.colloquial_version = None
         try:
             process = subprocess.run(["git", "rev-parse", "HEAD"],
                                      capture_output=True,
@@ -45,9 +36,8 @@ class ModuleCoSWID(object):
             "software-version": self.Version,
         }
 
-        if not self.colloquial_version is None:
+        if self.colloquial_version is not None:
             config["uSWID"]["colloquial-version"] = self.colloquial_version
 
         with open(self.FilePath, 'w') as f:
             config.write(f)
-
