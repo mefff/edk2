1ca369ba415b586d6c6b86d4f9da9df895ab2c88
Martin Fernandez <martin.fernandez@eclypsium.com>
2022-05-30 14:28:15 -0300

    Take into account the header files too for the edition hash

--- scripts/edition.sh
+++ scripts/edition.sh
@@ -10,11 +10,13 @@ module_name="${2:?}"
 edition=$(
     ar tf "${output_dir}/${module_name}.lib" | while read -a obj; do
 	cat "${output_dir}/${obj[@]}.src"
+	cat <(tr ' ' '\n' < "${output_dir}/headers.txt")
     done | \
 	sort | \
 	tee "log/${module_name}.files" | \
 	xargs cat | \
-	sha256sum
+	sha256sum | \
+	cut -d' ' -f 1
     )
 
 cat <<EOF > "${output_dir}/${module_name}.ini"
